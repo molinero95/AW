fd7e0be0c6d44b2c945d89c5349481508a713f7c
molinero95 <jaimolas95@gmail.com>
2017-10-23 21:27:55 +0200

    Reorganizacion
    FINAL

--- Entrega3/tareas.js
+++ Entrega3/tareas.js
@@ -20,36 +20,49 @@ let listaTareas = [
  * Devuelve las tareas de la lista de entrada que no hayan sido finalizadas.
  */
 function getToDoTasks(tasks) {
-  /* Implementar */
+	return tasks.filter(n=>n.done === false || n.done === undefined).map(n=>n.text);
 }
 
 /**
  * Devuelve las tareas que contengan el tag especificado
  */
 function findByTag(tasks, tag) {
-  /* Implementar */
+	return tasks.filter(n=>n.tags.indexOf(tag) !== -1);
 }
 
 /**
  * Devuelve las tareas que contengan alguno de los tags especificados
  */
 function findByTags(tasks, tags) {
-  /* Implementar */
+	return tasks.filter(n=>n.tags.some(n=>tags.indexOf(n) !== -1));
 }
 
 /**
  * Devuelve el número de tareas finalizadas
  */
 function countDone(tasks) {
-  /* Implementar */
+	return tasks.filter(n=>n.done === true).reduce((ac, n) => ac + 1, 0);
 }
 
 /**
  * Construye una tarea a partir de un texto con tags de la forma "@tag"
  */
 function createTask(text) {
-  /* Implementar */
+	let exp = /\@[A-z]{0,}/g;
+	let nuevaCadena = text;
+	let tarea, etiq, simple;
+	tarea = {text:"", tags: []}
+	while(etiq = exp.exec(text)){
+		nuevaCadena = nuevaCadena.replace(etiq[0], "");
+		simple = etiq[0].replace("@","");
+		tarea.tags.push(simple);
+	}
+	nuevaCadena = nuevaCadena.trim();
+	tarea["text"] = nuevaCadena;
+	return tarea;
 }
+
+console.log(createTask("hola mundo"));
 
 
 /*
