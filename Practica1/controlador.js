a830968df5a58a7acf3af1aed3333dfe747a17bb
molinero95 <jaimolas95@gmail.com>
2017-11-21 00:27:07 +0100

    Reorganización proyecto y creacion de ejs

--- Practica1/controlador.js
+++ Practica1/controlador.js
@@ -4,7 +4,26 @@ const path = require('path');
 const app = express();
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "views"));
+const estaticos = path.join(__dirname, "static");
+app.use(express.static(estaticos));
+
+app.get('/', (req, res) => {
+    res.status(200);
+    res.redirect('/profile.html');
+})
 
 app.get('/profile.html', (req, res) => {
+    let usuario = new Object();
+    usuario.nombre = "Pepito Pérez Rodriguez";
+    usuario.edad = 33;
+    usuario.sexo = "Hombre";
+    usuario.puntos = 100;
+    usuario.foto = "icons/Fatso-01.png";
+    //el render se encarga de llamar al .ejs
+    res.render("profile", {user: usuario})
     res.status(200);
+});
+
+app.listen(3000, () => {
+    console.log(`Server started on port`);
 });
